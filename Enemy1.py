5baea5d27d1dc219e6cfa8fcb68e4d4ede328b88
Jay4769 <famquanganh@gmail.com>
2023-12-20 16:20:10 +0300

    Final

--- Enemy1.py
+++ Enemy1.py
@@ -1,14 +1,15 @@
 class Enemy:
+    """Represents an enemy character in the game"""
+
     def __init__(self, enemy_pos_x, enemy_pos_y, alive):
         """Initializes an instance of the Enemy class
 
-                :param enemy_pos_x: x-coordinate of the enemy's position
-                :type enemy_pos_x: float
-                :param enemy_pos_y: y-coordinate of the enemy's position
-                :type enemy_pos_y: float
-                :param alive: determines the life status of the enemy
-                :type alive: bool"""
-
+            :param enemy_pos_x: x-coordinate of the enemy's position
+            :type enemy_pos_x: float
+            :param enemy_pos_y: y-coordinate of the enemy's position
+            :type enemy_pos_y: float
+            :param alive: determines the life status of the enemy
+            :type alive: bool"""
 
         self.image_l = []
         self.image_r = []
@@ -29,9 +30,8 @@ class Enemy:
         self.alive = alive
         self.hp = 1
 
-
     def movement_plus_anim_enemys(self, player_x: float, player_y: float, screen):
-        """"Forces enemies to move towards the player and changes enemy's animation
+        """Forces enemies to move towards the player and changes enemy's animation
 
             :param player_x: x-coordinate 
             :type player_x: float
@@ -45,7 +45,7 @@ class Enemy:
             :otype: Surface"""
 
         if self.alive == 1:
-            if not(abs(self.enemy_pos_x - player_x) <= 25 and abs(self.enemy_pos_y - player_y) <= 50):
+            if not (abs(self.enemy_pos_x - player_x) <= 25 and abs(self.enemy_pos_y - player_y) <= 50):
                 if -70 <= player_x - self.enemy_pos_x <= 70 and self.enemy_pos_y - player_y > 50:
                     screen.blit(self.image_u[self.enemy_anim_count], (self.enemy_pos_x, self.enemy_pos_y))
                 elif -70 <= player_x - self.enemy_pos_x <= 70 and player_y - self.enemy_pos_y > 50:
@@ -62,7 +62,8 @@ class Enemy:
                     self.enemy_pos_y -= 3 / 2**0.5
 
                 if self.repeat_enemy_anim == 4:
-                    self.enemy_anim_count += (self.enemy_anim_count != 7) - self.enemy_anim_count*(self.enemy_anim_count == 7)
+                    self.enemy_anim_count += ((self.enemy_anim_count != 7) -
+                                              self.enemy_anim_count*(self.enemy_anim_count == 7))
                     self.repeat_enemy_anim = 0
                 self.repeat_enemy_anim += 1
             else:
@@ -71,7 +72,8 @@ class Enemy:
                 if self.enemy_pos_x > player_x:
                     screen.blit(self.enemy_fight_l[self.enemy_fight_anim_count], (self.enemy_pos_x, self.enemy_pos_y))
                 if self.repeat_anim_fight == 4:
-                    self.enemy_fight_anim_count += (self.enemy_fight_anim_count != 5) - self.enemy_fight_anim_count*(self.enemy_fight_anim_count == 5)
+                    self.enemy_fight_anim_count += ((self.enemy_fight_anim_count != 5) -
+                                                    self.enemy_fight_anim_count*(self.enemy_fight_anim_count == 5))
                     self.repeat_anim_fight = 0
                 self.repeat_anim_fight += 1
 
